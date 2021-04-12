d36c5f08a6724f6124d80d2b95e2cbb9b236fcf4
Brian Sales <mr.brian.mrs.kathleen@gmail.com>
2021-04-12 16:33:00 -0500

    got the generate html section working

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,14 +1,43 @@
 function renderEmployeestatus(employee){
+    var html = ''
 
     for (i = 0; i < employee.length; i++) {
         
         if(employee[i].type === 'Manager'){
 
         
-            return `<div>
-            ${employee[i].name}
-            ${employee[i].email}
-            ${employee[i].officeNumber}
+            html = html + `<div class="employeeContainer">
+            <p>${employee[i].type}</p>
+            <p>${employee[i].id}</p>
+            <p>${employee[i].name}</p>
+            <p>${employee[i].email}</p>
+            <p>${employee[i].officeNumber}</p>
+            
+            </div>`
+    
+            
+        } else if (employee[i].type === 'Engineer'){
+
+        
+            html = html + `<div class="employeeContainer">
+            <p>${employee[i].type}</p>
+            <p>${employee[i].id}</p>
+            <p>${employee[i].name}</p>
+            <p>${employee[i].email}</p>
+            <p>${employee[i].github}</p>
+            
+            </div>`
+    
+            
+        } else if (employee[i].type === 'Intern'){
+
+        
+            html = html + `<div class="employeeContainer">
+            <p>${employee[i].type}</p>
+            <p>${employee[i].id}</p>
+            <p>${employee[i].name}</p>
+            <p>${employee[i].email}</p>
+            <p>${employee[i].school}</p>
             
             </div>`
     
@@ -16,6 +45,7 @@ function renderEmployeestatus(employee){
         }
 
       }
+      return html;
 
 
     
@@ -34,6 +64,7 @@ function generateHTML(data){
         <title>Team Profile</title>
     </head>
     <body>
+        <div class="header">
         ${renderEmployeestatus(data)}
         
     </body>
